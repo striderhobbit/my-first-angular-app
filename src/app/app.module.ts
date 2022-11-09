3e7efb52b529814e1dd7f94d055fd4e4f4d4bbbf
Marius Köhler <marius.koehler@be-terna.com>
2022-11-09 17:15:13 +0100

    material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { TopBarComponentComponent } from './top-bar-component/top-bar-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponentComponent
   ],
   imports: [
     BrowserModule
